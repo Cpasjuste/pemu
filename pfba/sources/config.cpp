f172453048375cce9a1f60449519b6115d67319f
Cpasjuste <cpasjuste@gmail.com>
2019-09-27 09:21:34 +0200

    update for latest cross2d changes

--- pfba/sources/config.cpp
+++ pfba/sources/config.cpp
@@ -16,19 +16,19 @@ using namespace c2dui;
 
 PFBAConfig::PFBAConfig(c2d::Io *io, int version) : Config(io, version) {
 
-    printf("PFBAConfig(%s, v%i)\n", io->getDataWritePath().c_str(), version);
+    printf("PFBAConfig(%s, v%i)\n", io->getDataPath().c_str(), version);
 
     // add fba default roms paths
-    getRomPaths()->emplace_back(io->getDataWritePath() + "coleco/");
-    getRomPaths()->emplace_back(io->getDataWritePath() + "gamegear/");
-    getRomPaths()->emplace_back(io->getDataWritePath() + "megadriv/");
-    getRomPaths()->emplace_back(io->getDataWritePath() + "msx/");
-    getRomPaths()->emplace_back(io->getDataWritePath() + "pce/");
-    getRomPaths()->emplace_back(io->getDataWritePath() + "sg1000/");
-    getRomPaths()->emplace_back(io->getDataWritePath() + "sgx/");
-    getRomPaths()->emplace_back(io->getDataWritePath() + "sms/");
-    getRomPaths()->emplace_back(io->getDataWritePath() + "tg16/");
-    getRomPaths()->emplace_back(io->getDataWritePath() + "spectrum/");
+    getRomPaths()->emplace_back(io->getDataPath() + "coleco/");
+    getRomPaths()->emplace_back(io->getDataPath() + "gamegear/");
+    getRomPaths()->emplace_back(io->getDataPath() + "megadriv/");
+    getRomPaths()->emplace_back(io->getDataPath() + "msx/");
+    getRomPaths()->emplace_back(io->getDataPath() + "pce/");
+    getRomPaths()->emplace_back(io->getDataPath() + "sg1000/");
+    getRomPaths()->emplace_back(io->getDataPath() + "sgx/");
+    getRomPaths()->emplace_back(io->getDataPath() + "sms/");
+    getRomPaths()->emplace_back(io->getDataPath() + "tg16/");
+    getRomPaths()->emplace_back(io->getDataPath() + "spectrum/");
     for (size_t i = getRomPaths()->size(); i < 20; i++) {
         getRomPaths()->emplace_back(std::string());
     }
