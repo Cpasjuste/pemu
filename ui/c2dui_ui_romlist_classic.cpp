1e36c14b1f6689d5359e0b5b66451df082bafb36
cpasjuste <cpasjuste@gmail.com>
2019-12-06 21:11:36 +0100

    ui: do not play video snap... if video snap is missing

--- ui/c2dui_ui_romlist_classic.cpp
+++ ui/c2dui_ui_romlist_classic.cpp
@@ -108,10 +108,12 @@ public:
 #ifdef __MPV__
         std::string romPath = ui->getConfig()->getRomPath(0);
         std::string path = romPath + game.getMedia("video").url;
-        int res = mpv->load(path, Mpv::LoadType::Replace, "loop=yes");
-        if (res == 0) {
-            mpvTexture->setLayer(1);
-            mpvTexture->setVisibility(Visibility::Visible, false);
+        if (ui->getIo()->exist(path)) {
+            int res = mpv->load(path, Mpv::LoadType::Replace, "loop=yes");
+            if (res == 0) {
+                mpvTexture->setLayer(1);
+                mpvTexture->setVisibility(Visibility::Visible, false);
+            }
         }
 #endif
         return true;
