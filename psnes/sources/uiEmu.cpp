8a7ab56817903f1452dc21fdec3ef456021d8f9e
cpasjuste <cpasjuste@gmail.com>
2021-03-09 10:01:10 +0100

    psnes: update snes9x core to latest git revision (8a5d29c)

--- psnes/sources/uiEmu.cpp
+++ psnes/sources/uiEmu.cpp
@@ -504,60 +504,6 @@ bool8 S9xContinueUpdate(int width, int height) {
     return TRUE;
 }
 
-void _splitpath(const char *path, char *drive, char *dir, char *fname, char *ext) {
-
-    *drive = 0;
-
-    const char *slash = strrchr(path, SLASH_CHAR);
-    const char *dot = strrchr(path, '.');
-
-    if (dot && slash && dot < slash) {
-        dot = nullptr;
-    }
-
-    if (!slash) {
-        *dir = 0;
-
-        strcpy(fname, path);
-
-        if (dot) {
-            fname[dot - path] = 0;
-            strcpy(ext, dot + 1);
-        } else {
-            *ext = 0;
-        }
-    } else {
-        strcpy(dir, path);
-        dir[slash - path] = 0;
-
-        strcpy(fname, slash + 1);
-
-        if (dot) {
-            fname[dot - slash - 1] = 0;
-            strcpy(ext, dot + 1);
-        } else {
-            *ext = 0;
-        }
-    }
-}
-
-void _makepath(char *path, const char *, const char *dir, const char *fname, const char *ext) {
-
-    if (dir && *dir) {
-        strcpy(path, dir);
-        strcat(path, SLASH_STR);
-    } else {
-        *path = 0;
-    }
-
-    strcat(path, fname);
-
-    if (ext && *ext) {
-        strcat(path, ".");
-        strcat(path, ext);
-    }
-}
-
 /**
  * Display port-specific usage information
  */
