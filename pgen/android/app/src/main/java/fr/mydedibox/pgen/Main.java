5ab5b0dbd213eed1d267b9b9f67178a8b6a5b619
cpasjuste <cpasjuste@gmail.com>
2022-06-15 10:49:08 +0200

    android: add pgen (wip)

---
+++ pgen/android/app/src/main/java/fr/mydedibox/pgen/Main.java
@@ -0,0 +1,13 @@
+package fr.mydedibox.pgen;
+
+import fr.mydedibox.libcross2d.*;
+
+public class Main extends C2DMainActivity {
+
+    @Override
+    protected String[] getLibraries() {
+        return new String[]{
+                "pgen"
+        };
+    }
+}
