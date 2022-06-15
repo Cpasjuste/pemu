df7cf8a1ddb809ee28805edef566f319fe252807
cpasjuste <cpasjuste@gmail.com>
2022-06-15 10:42:13 +0200

    android: add pnes (wip)

---
+++ pnes/android/app/src/main/java/fr/mydedibox/pnes/Main.java
@@ -0,0 +1,13 @@
+package fr.mydedibox.pnes;
+
+import fr.mydedibox.libcross2d.*;
+
+public class Main extends C2DMainActivity {
+
+    @Override
+    protected String[] getLibraries() {
+        return new String[]{
+                "pnes"
+        };
+    }
+}
