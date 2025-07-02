5c28c259042798de1beb1d42c881f36e21a95f2a
cpasjuste <cpasjuste@gmail.com>
2025-07-02 11:12:48 +0200

    - pgen: fix for latest libcross2d changes - cmake: bump cmake minimum required version

--- src/cores/pgen/sources/pgen_ui_emu.cpp
+++ src/cores/pgen/sources/pgen_ui_emu.cpp
@@ -15,14 +15,14 @@ static short sound_buffer[2048];
 
 static uint32_t brm_crc[2];
 static uint8 brm_format[0x40] = {
-        0x5f, 0x5f, 0x5f, 0x5f, 0x5f, 0x5f, 0x5f, 0x5f, 0x5f, 0x5f, 0x5f, 0x00, 0x00, 0x00, 0x00,
-        0x40,
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-        0x00,
-        0x53, 0x45, 0x47, 0x41, 0x5f, 0x43, 0x44, 0x5f, 0x52, 0x4f, 0x4d, 0x00, 0x01, 0x00, 0x00,
-        0x00,
-        0x52, 0x41, 0x4d, 0x5f, 0x43, 0x41, 0x52, 0x54, 0x52, 0x49, 0x44, 0x47, 0x45, 0x5f, 0x5f,
-        0x5f
+    0x5f, 0x5f, 0x5f, 0x5f, 0x5f, 0x5f, 0x5f, 0x5f, 0x5f, 0x5f, 0x5f, 0x00, 0x00, 0x00, 0x00,
+    0x40,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00,
+    0x53, 0x45, 0x47, 0x41, 0x5f, 0x43, 0x44, 0x5f, 0x52, 0x4f, 0x4d, 0x00, 0x01, 0x00, 0x00,
+    0x00,
+    0x52, 0x41, 0x4d, 0x5f, 0x43, 0x41, 0x52, 0x54, 0x52, 0x49, 0x44, 0x47, 0x45, 0x5f, 0x5f,
+    0x5f
 };
 
 PGENUiEmu::PGENUiEmu(UiMain *ui) : UiEmu(ui) {
@@ -47,9 +47,9 @@ int PGENUiEmu::load(const ss_api::Game &game) {
             || !pMain->getIo()->exist(CD_BIOS_JP)) {
             getUi()->getUiProgressBox()->setVisibility(Visibility::Hidden);
             getUi()->getUiMessageBox()->show(
-                    "WARNING",
-                    "MISSING MEGA-CD BIOS DETECTED, EMULATION WILL LIKELY NOT WORK...",
-                    "OK");
+                "WARNING",
+                "MISSING MEGA-CD BIOS DETECTED, EMULATION WILL LIKELY NOT WORK...",
+                "OK");
         }
     }
 
@@ -65,7 +65,7 @@ int PGENUiEmu::load(const ss_api::Game &game) {
     if (!load_rom((char *) path.c_str())) {
         getUi()->getUiProgressBox()->setVisibility(Visibility::Hidden);
         getUi()->getUiMessageBox()->show(
-                "ERROR", "INVALID FILE OR MISSING BIOS...", "OK");
+            "ERROR", "INVALID FILE OR MISSING BIOS...", "OK");
         stop();
         return -1;
     }
@@ -132,7 +132,6 @@ void PGENUiEmu::onUpdate() {
                 default:
                     break;
             }
-
         }
 
         if (system_hw != SYSTEM_GG && system_hw != SYSTEM_GGMS) {
@@ -202,13 +201,8 @@ void PGENUiEmu::loadBios() {
     system_bios = 0;
 
     /* Genesis BOOT ROM support (2KB max) */
-    char *boot_rom_data;
-    memset(boot_rom, 0xFF, 0x800);
-    size_t size = getUi()->getIo()->read(MD_BIOS, &boot_rom_data, 0x800);
-    if (boot_rom_data && size == 0x800) {
-        memcpy(boot_rom, boot_rom_data, 0x800);
-        free(boot_rom_data);
-
+    const auto size = getUi()->getIo()->read(MD_BIOS, (char *) boot_rom, 0x800);
+    if (size > 0) {
         /* check BOOT ROM */
         if (!memcmp((char *) (boot_rom + 0x120), "GENESIS OS", 10)) {
             /* mark Genesis BIOS as loaded */
@@ -218,12 +212,10 @@ void PGENUiEmu::loadBios() {
 
         /* Byteswap ROM */
         for (int i = 0; i < 0x800; i += 2) {
-            uint8 temp = boot_rom[i];
+            const auto temp = boot_rom[i];
             boot_rom[i] = boot_rom[i + 1];
             boot_rom[i + 1] = temp;
         }
-    } else if (boot_rom_data) {
-        free(boot_rom_data);
     }
 }
 
@@ -232,23 +224,15 @@ void PGENUiEmu::loadBram() {
         return;
     }
 
-    char *data;
     std::string ramPath = pMain->getIo()->getDataPath() + "rams/";
     size_t size = getUi()->getIo()->read(ramPath + Utility::removeExt(currentGame.path) + ".brm",
-                                         &data, 0x2000);
-    if (data && size == 0x2000) {
-        memcpy(scd.bram, data, 0x2000);
-        free(data);
-        data = nullptr;
+                                         (char *) scd.bram, 0x2000);
+    if (size > 1) {
         /* update CRC */
         brm_crc[0] = crc32(0, scd.bram, 0x2000);
     } else {
         /* force internal backup RAM format (does not use previous region backup RAM) */
         scd.bram[0x1fff] = 0;
-        if (data) {
-            free(data);
-            data = nullptr;
-        }
     }
 
     /* check if internal backup RAM is correctly formatted */
@@ -259,7 +243,7 @@ void PGENUiEmu::loadBram() {
         /* internal Backup RAM size fields */
         brm_format[0x10] = brm_format[0x12] = brm_format[0x14] = brm_format[0x16] = 0x00;
         brm_format[0x11] = brm_format[0x13] = brm_format[0x15] = brm_format[0x17] =
-                (sizeof(scd.bram) / 64) - 3;
+                                                                 (sizeof(scd.bram) / 64) - 3;
 
         /* format internal backup RAM */
         memcpy(scd.bram + 0x2000 - 0x40, brm_format, 0x40);
@@ -271,13 +255,9 @@ void PGENUiEmu::loadBram() {
     /* automatically load cartridge backup RAM (if enabled) */
     if (scd.cartridge.id) {
         // TODO: read chunks
-        size = getUi()->getIo()->read(ramPath + CD_BRAM_CART, &data, scd.cartridge.mask + 1);
-        if (data && size == scd.cartridge.mask + 1) {
-            memcpy(scd.cartridge.area, data, scd.cartridge.mask + 1);
-            free(data);
+        size = getUi()->getIo()->read(ramPath + CD_BRAM_CART, (char *) scd.cartridge.area, scd.cartridge.mask + 1);
+        if (size == scd.cartridge.mask + 1) {
             brm_crc[1] = crc32(0, scd.cartridge.area, scd.cartridge.mask + 1);
-        } else if (data) {
-            free(data);
         }
 
         /* check if cartridge backup RAM is correctly formatted */
@@ -288,9 +268,9 @@ void PGENUiEmu::loadBram() {
 
             /* Cartridge Backup RAM size fields */
             brm_format[0x10] = brm_format[0x12] = brm_format[0x14] = brm_format[0x16] =
-                    (((scd.cartridge.mask + 1) / 64) - 3) >> 8;
+                                                                     (((scd.cartridge.mask + 1) / 64) - 3) >> 8;
             brm_format[0x11] = brm_format[0x13] = brm_format[0x15] = brm_format[0x17] =
-                    (((scd.cartridge.mask + 1) / 64) - 3) & 0xff;
+                                                                     (((scd.cartridge.mask + 1) / 64) - 3) & 0xff;
 
             /* format cartridge backup RAM */
             memcpy(scd.cartridge.area + scd.cartridge.mask + 1 - 0x40, brm_format, 0x40);
@@ -330,15 +310,11 @@ void PGENUiEmu::saveBram() {
 
 void PGENUiEmu::loadSram() {
     if (sram.on) {
-        char *data;
-        std::string ramPath = pMain->getIo()->getDataPath() + "rams/";
-        size_t size = getUi()->getIo()->read(
-                ramPath + Utility::removeExt(currentGame.path) + ".srm", &data, 0x10000);
-        if (data && size == 0x10000) {
-            memcpy(sram.sram, data, 0x10000);
-        }
-        if (data) {
-            free(data);
+        const auto ramPath = pMain->getIo()->getDataPath() + "rams/";
+        const auto size = getUi()->getIo()->read(
+            ramPath + Utility::removeExt(currentGame.path) + ".srm", (char *) sram.sram, 0x10000);
+        if (size < 0x10000) {
+            memset(sram.sram, 0x00, 0x10000);
         }
     }
 }
--- src/cores/pgen/sources/pgen_ui_menu_state.cpp
+++ src/cores/pgen/sources/pgen_ui_menu_state.cpp
@@ -10,24 +10,25 @@ extern "C" {
 }
 
 bool PGENUIStateMenu::loadStateCore(const char *path) {
-    unsigned char *buf = nullptr;
-    bool success = getUi()->getIo()->read(path, (char **) &buf, STATE_SIZE);
-    if (buf && success) {
-        success = state_load(buf);
+    bool ret = false;
+    const auto buf = (uint8_t *) malloc(STATE_SIZE);
+    const auto size = getUi()->getIo()->read(path, (char *) buf, STATE_SIZE);
+    if (size == STATE_SIZE) {
+        ret = state_load(buf);
     }
 
     if (buf) {
         free(buf);
     }
 
-    return success;
+    return ret;
 }
 
 bool PGENUIStateMenu::saveStateCore(const char *path) {
-    auto buf = (uint8 *) malloc(STATE_SIZE);
+    const auto buf = (uint8_t *) malloc(STATE_SIZE);
     bool ret = false;
     if (buf) {
-        int len = state_save(buf);
+        const auto len = state_save(buf);
         ret = getUi()->getIo()->write(path, (const char *) buf, len);
         free(buf);
     }
