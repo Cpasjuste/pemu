61b36fbbc36b760a914afa5c0602c6c61c4907fe
mylinc <wxj.linc@gmail.com>
2026-08-12 12:37:23 +0800

    XY

--- src/cores/pnes/sources/pnes_ui_emu.cpp
+++ src/cores/pnes/sources/pnes_ui_emu.cpp
@@ -9,7 +9,6 @@
 #include "fltkui/config.h"
 #include "fltkui/audio.h"
 #include "fltkui/input.h"
-
 #include "skeleton/pemu.h"
 #include "pnes_ui_emu.h"
 
@@ -80,49 +79,102 @@ bool PNESUiEmu::onInput(c2d::Input::Player *players) {
 }
 
 void PNESUiEmu::onUpdate() {
+    static unsigned int turboCounter = 0;
+
     if (!isPaused()) {
+        // Turbo pulse:
+        // timing_turbopulse = 3 means:
+        // 3 frames ON, 3 frames OFF.
+        // NTSC 60 fps -> about 10 presses/sec.
+        // PAL  50 fps -> about 8.3 presses/sec.
+        const unsigned int turboPulse =
+            conf.timing_turbopulse > 0 ?
+            static_cast<unsigned int>(conf.timing_turbopulse) : 3U;
+
+        const bool turboOn =
+            ((turboCounter / turboPulse) & 1U) == 0;
+
+        turboCounter++;
+
         // update nestopia buttons
         auto *players = getUi()->getInput()->getPlayers();
-
-        if (players[0].buttons & c2d::Input::Button::X) {
-            nst_set_rewind(0);
-        } else if (Rewinder(emulator).GetDirection() == Rewinder::BACKWARD) {
-            nst_set_rewind(1);
-        }
 
         for (int i = 0; i < NUMGAMEPADS; i++) {
             cNstPads->pad[i].buttons = 0;
 
-            cNstPads->pad[i].buttons |= (players[i].buttons & c2d::Input::Button::Start) > 0 ?
-                                        Nes::Core::Input::Controllers::Pad::START : 0;
-            cNstPads->pad[i].buttons |= (players[i].buttons & c2d::Input::Button::Select) > 0 ?
-                                        Nes::Core::Input::Controllers::Pad::SELECT : 0;
-            cNstPads->pad[i].buttons |= (players[i].buttons & c2d::Input::Button::Up) > 0 ?
-                                        Nes::Core::Input::Controllers::Pad::UP : 0;
-            cNstPads->pad[i].buttons |= (players[i].buttons & c2d::Input::Button::Down) > 0 ?
-                                        Nes::Core::Input::Controllers::Pad::DOWN : 0;
-            cNstPads->pad[i].buttons |= (players[i].buttons & c2d::Input::Button::Left) > 0 ?
-                                        Nes::Core::Input::Controllers::Pad::LEFT : 0;
-            cNstPads->pad[i].buttons |= (players[i].buttons & c2d::Input::Button::Right) > 0 ?
-                                        Nes::Core::Input::Controllers::Pad::RIGHT : 0;
+            cNstPads->pad[i].buttons |=
+                (players[i].buttons & c2d::Input::Button::Start) > 0 ?
+                Nes::Core::Input::Controllers::Pad::START : 0;
+
+            cNstPads->pad[i].buttons |=
+                (players[i].buttons & c2d::Input::Button::Select) > 0 ?
+                Nes::Core::Input::Controllers::Pad::SELECT : 0;
+
+            cNstPads->pad[i].buttons |=
+                (players[i].buttons & c2d::Input::Button::Up) > 0 ?
+                Nes::Core::Input::Controllers::Pad::UP : 0;
+
+            cNstPads->pad[i].buttons |=
+                (players[i].buttons & c2d::Input::Button::Down) > 0 ?
+                Nes::Core::Input::Controllers::Pad::DOWN : 0;
+
+            cNstPads->pad[i].buttons |=
+                (players[i].buttons & c2d::Input::Button::Left) > 0 ?
+                Nes::Core::Input::Controllers::Pad::LEFT : 0;
+
+            cNstPads->pad[i].buttons |=
+                (players[i].buttons & c2d::Input::Button::Right) > 0 ?
+                Nes::Core::Input::Controllers::Pad::RIGHT : 0;
+
 #ifdef __3DS__
-            cNstPads->pad[i].buttons |= (players[i].buttons & c2d::Input::Button::A) > 0 ?
-                                        Nes::Core::Input::Controllers::Pad::A : 0;
-            cNstPads->pad[i].buttons |= (players[i].buttons & c2d::Input::Button::B) > 0 ?
-                                        Nes::Core::Input::Controllers::Pad::B : 0;
-            cNstPads->pad[i].buttons |= (players[i].buttons & c2d::Input::Button::X) > 0 ?
-                                        Nes::Core::Input::Controllers::Pad::A : 0;
-            cNstPads->pad[i].buttons |= (players[i].buttons & c2d::Input::Button::Y) > 0 ?
-                                        Nes::Core::Input::Controllers::Pad::B : 0;
+
+            cNstPads->pad[i].buttons |=
+                (players[i].buttons & c2d::Input::Button::A) > 0 ?
+                Nes::Core::Input::Controllers::Pad::A : 0;
+
+            cNstPads->pad[i].buttons |=
+                (players[i].buttons & c2d::Input::Button::B) > 0 ?
+                Nes::Core::Input::Controllers::Pad::B : 0;
+
+            cNstPads->pad[i].buttons |=
+                (players[i].buttons & c2d::Input::Button::X) > 0 ?
+                Nes::Core::Input::Controllers::Pad::A : 0;
+
+            cNstPads->pad[i].buttons |=
+                (players[i].buttons & c2d::Input::Button::Y) > 0 ?
+                Nes::Core::Input::Controllers::Pad::B : 0;
+
 #else
-            cNstPads->pad[i].buttons |= (players[i].buttons & c2d::Input::Button::A) > 0 ?
-                                        Nes::Core::Input::Controllers::Pad::B : 0;
-            cNstPads->pad[i].buttons |= (players[i].buttons & c2d::Input::Button::B) > 0 ?
-                                        Nes::Core::Input::Controllers::Pad::A : 0;
-            cNstPads->pad[i].buttons |= (players[i].buttons & c2d::Input::Button::X) > 0 ?
-                                        Nes::Core::Input::Controllers::Pad::B : 0;
-            cNstPads->pad[i].buttons |= (players[i].buttons & c2d::Input::Button::Y) > 0 ?
-                                        Nes::Core::Input::Controllers::Pad::A : 0;
+
+            // Nintendo Switch / other non-3DS platforms
+            //
+            // B = NES A
+            // A = NES B
+            // Y = Turbo A
+            // X = Turbo B
+
+            // Normal NES A
+            cNstPads->pad[i].buttons |=
+                (players[i].buttons & c2d::Input::Button::B) > 0 ?
+                Nes::Core::Input::Controllers::Pad::A : 0;
+
+            // Normal NES B
+            cNstPads->pad[i].buttons |=
+                (players[i].buttons & c2d::Input::Button::A) > 0 ?
+                Nes::Core::Input::Controllers::Pad::B : 0;
+
+            if (turboOn) {
+                // Y = Turbo NES A
+                cNstPads->pad[i].buttons |=
+                    (players[i].buttons & c2d::Input::Button::Y) > 0 ?
+                    Nes::Core::Input::Controllers::Pad::A : 0;
+
+                // X = Turbo NES B
+                cNstPads->pad[i].buttons |=
+                    (players[i].buttons & c2d::Input::Button::X) > 0 ?
+                    Nes::Core::Input::Controllers::Pad::B : 0;
+            }
+
 #endif
         }
 
@@ -136,6 +188,7 @@ void PNESUiEmu::onUpdate() {
 /// NESTOPIA
 
 /// NESTOPIA AUDIO
+
 static void *audio_buffer = nullptr;
 
 void audio_init() {
@@ -152,18 +205,31 @@ void audio_deinit() {
 
 void audio_queue() {
     Audio *audio = uiEmu->getAudio();
+
     if (audio) {
 #if 0
         int samples = uiEmu->getAudio()->getSamples();
         int queued = audio->getSampleBufferQueued();
         int capacity = audio->getSampleBufferCapacity();
+
         if (samples + queued > capacity) {
-            printf("WARNING: samples: %i, queued: %i, capacity: %i (fps: %i)\n",
-                   samples, queued, capacity, nst_pal() ? 50 : 60);
+            printf(
+                "WARNING: samples: %i, queued: %i, capacity: %i (fps: %i)\n",
+                samples,
+                queued,
+                capacity,
+                nst_pal() ? 50 : 60
+            );
         }
 #endif
-        uiEmu->getAudio()->play(audio_buffer, uiEmu->getAudio()->getSamples(),
-                                nst_pal() ? Audio::SyncMode::LowLatency : Audio::SyncMode::None);
+
+        uiEmu->getAudio()->play(
+            audio_buffer,
+            uiEmu->getAudio()->getSamples(),
+            nst_pal() ?
+            Audio::SyncMode::LowLatency :
+            Audio::SyncMode::None
+        );
     }
 }
 
@@ -185,14 +251,15 @@ void audio_set_speed(int speed) {
 }
 
 void audio_set_params(Sound::Output *soundoutput) {
-
     Audio *aud = uiEmu->getAudio();
 
     if (aud) {
         // Set audio parameters
         Sound sound(emulator);
 
-        sound.SetSampleRate((unsigned long) conf.audio_sample_rate);
+        sound.SetSampleRate(
+            static_cast<unsigned long>(conf.audio_sample_rate)
+        );
 
         sound.SetSpeaker(Sound::SPEAKER_STEREO);
         sound.SetSpeed(Sound::DEFAULT_SPEED);
@@ -200,9 +267,17 @@ void audio_set_params(Sound::Output *soundoutput) {
         audio_adj_volume();
 
         audio_buffer = malloc(aud->getSamplesSize());
-        memset(audio_buffer, 0, aud->getSamplesSize());
+
+        memset(
+            audio_buffer,
+            0,
+            aud->getSamplesSize()
+        );
+
         soundoutput->samples[0] = audio_buffer;
-        soundoutput->length[0] = (unsigned int) aud->getSamples();
+        soundoutput->length[0] =
+            static_cast<unsigned int>(aud->getSamples());
+
         soundoutput->samples[1] = nullptr;
         soundoutput->length[1] = 0;
     }
@@ -211,39 +286,92 @@ void audio_set_params(Sound::Output *soundoutput) {
 void audio_adj_volume() {
     // Adjust the audio volume to the current settings
     Sound sound(emulator);
-    sound.SetVolume(Sound::ALL_CHANNELS, conf.audio_volume);
-    sound.SetVolume(Sound::CHANNEL_SQUARE1, conf.audio_vol_sq1);
-    sound.SetVolume(Sound::CHANNEL_SQUARE2, conf.audio_vol_sq2);
-    sound.SetVolume(Sound::CHANNEL_TRIANGLE, conf.audio_vol_tri);
-    sound.SetVolume(Sound::CHANNEL_NOISE, conf.audio_vol_noise);
-    sound.SetVolume(Sound::CHANNEL_DPCM, conf.audio_vol_dpcm);
-    sound.SetVolume(Sound::CHANNEL_FDS, conf.audio_vol_fds);
-    sound.SetVolume(Sound::CHANNEL_MMC5, conf.audio_vol_mmc5);
-    sound.SetVolume(Sound::CHANNEL_VRC6, conf.audio_vol_vrc6);
-    sound.SetVolume(Sound::CHANNEL_VRC7, conf.audio_vol_vrc7);
-    sound.SetVolume(Sound::CHANNEL_N163, conf.audio_vol_n163);
-    sound.SetVolume(Sound::CHANNEL_S5B, conf.audio_vol_s5b);
+
+    sound.SetVolume(
+        Sound::ALL_CHANNELS,
+        conf.audio_volume
+    );
+
+    sound.SetVolume(
+        Sound::CHANNEL_SQUARE1,
+        conf.audio_vol_sq1
+    );
+
+    sound.SetVolume(
+        Sound::CHANNEL_SQUARE2,
+        conf.audio_vol_sq2
+    );
+
+    sound.SetVolume(
+        Sound::CHANNEL_TRIANGLE,
+        conf.audio_vol_tri
+    );
+
+    sound.SetVolume(
+        Sound::CHANNEL_NOISE,
+        conf.audio_vol_noise
+    );
+
+    sound.SetVolume(
+        Sound::CHANNEL_DPCM,
+        conf.audio_vol_dpcm
+    );
+
+    sound.SetVolume(
+        Sound::CHANNEL_FDS,
+        conf.audio_vol_fds
+    );
+
+    sound.SetVolume(
+        Sound::CHANNEL_MMC5,
+        conf.audio_vol_mmc5
+    );
+
+    sound.SetVolume(
+        Sound::CHANNEL_VRC6,
+        conf.audio_vol_vrc6
+    );
+
+    sound.SetVolume(
+        Sound::CHANNEL_VRC7,
+        conf.audio_vol_vrc7
+    );
+
+    sound.SetVolume(
+        Sound::CHANNEL_N163,
+        conf.audio_vol_n163
+    );
+
+    sound.SetVolume(
+        Sound::CHANNEL_S5B,
+        conf.audio_vol_s5b
+    );
 }
 
 /// NESTOPIA CONFIG
-void PNESUiEmu::nestopia_config_init() {
 
+void PNESUiEmu::nestopia_config_init() {
     // Video
     conf.video_filter = 0;
     conf.video_scale_factor = 1;
     conf.video_palette_mode = 0;
     conf.video_decoder = 0;
-    conf.video_brightness = 0; // -100 to 100
-    conf.video_saturation = 0; // -100 to 100
-    conf.video_contrast = 0; // -100 to 100
-    conf.video_hue = 0; // -45 to 45
+
+    conf.video_brightness = 0;   // -100 to 100
+    conf.video_saturation = 0;   // -100 to 100
+    conf.video_contrast = 0;     // -100 to 100
+    conf.video_hue = 0;          // -45 to 45
+
     conf.video_ntsc_mode = 0;
-    conf.video_ntsc_sharpness = 0; // -100 to 100
-    conf.video_ntsc_resolution = 0; // -100 to 100
-    conf.video_ntsc_bleed = 0; // -100 to 100
-    conf.video_ntsc_artifacts = 0; // -100 to 100
-    conf.video_ntsc_fringing = 0; // -100 to 100
+
+    conf.video_ntsc_sharpness = 0;   // -100 to 100
+    conf.video_ntsc_resolution = 0;  // -100 to 100
+    conf.video_ntsc_bleed = 0;       // -100 to 100
+    conf.video_ntsc_artifacts = 0;   // -100 to 100
+    conf.video_ntsc_fringing = 0;    // -100 to 100
+
     conf.video_xbr_corner_rounding = 0;
+
     conf.video_linear_filter = false;
     conf.video_tv_aspect = false;
     conf.video_unmask_overscan = false;
@@ -255,7 +383,9 @@ void PNESUiEmu::nestopia_config_init() {
     // Audio
     conf.audio_stereo = true;
     conf.audio_sample_rate = 48000;
+
     conf.audio_volume = 85;
+
     conf.audio_vol_sq1 = 85;
     conf.audio_vol_sq2 = 85;
     conf.audio_vol_tri = 85;
@@ -271,6 +401,9 @@ void PNESUiEmu::nestopia_config_init() {
     // Timing
     conf.timing_speed = 60;
     conf.timing_ffspeed = 3;
+
+    // Turbo:
+    // 3 frames ON + 3 frames OFF
     conf.timing_turbopulse = 3;
 
     // Misc
@@ -286,14 +419,33 @@ void PNESUiEmu::nestopia_config_init() {
     conf.misc_homebrew_stderr = -1;
 }
 
-// NESTOPIA CORE INIT
-int PNESUiEmu::nestopia_core_init(const char *rom_path) {
+/// NESTOPIA CORE INIT
 
+int PNESUiEmu::nestopia_core_init(const char *rom_path) {
     // Set up directories (nst_set_dirs)
-    std::string data_path = pMain->getIo()->getDataPath();
-    strncpy(nstpaths.nstdir, data_path.c_str(), sizeof(nstpaths.nstdir));
-    strncpy(nstpaths.nstconfdir, data_path.c_str(), sizeof(nstpaths.nstconfdir));
-    snprintf(nstpaths.palettepath, sizeof(nstpaths.palettepath), "%s%s", nstpaths.nstdir, "custom.pal");
+    std::string data_path =
+        pMain->getIo()->getDataPath();
+
+    strncpy(
+        nstpaths.nstdir,
+        data_path.c_str(),
+        sizeof(nstpaths.nstdir)
+    );
+
+    strncpy(
+        nstpaths.nstconfdir,
+        data_path.c_str(),
+        sizeof(nstpaths.nstconfdir)
+    );
+
+    snprintf(
+        nstpaths.palettepath,
+        sizeof(nstpaths.palettepath),
+        "%s%s",
+        nstpaths.nstdir,
+        "custom.pal"
+    );
+
     // Create directories
     pMain->getIo()->create(data_path);
     pMain->getIo()->create(data_path + "save");
@@ -308,12 +460,14 @@ int PNESUiEmu::nestopia_core_init(const char *rom_path) {
     nst_input_turbo_init();
 
     // Set archive handler function pointer
-    nst_archive_select = &nst_archive_select_file;
+    nst_archive_select =
+        &nst_archive_select_file;
 
     // Set the video dimensions
     video_set_dimensions();
 
-    // Initialize and load FDS BIOS and NstDatabase.xml
+    // Initialize and load FDS BIOS,
+    // NstDatabase.xml, and custom palette
     nst_fds_bios_load();
     nst_db_load();
 
@@ -321,7 +475,9 @@ int PNESUiEmu::nestopia_core_init(const char *rom_path) {
         return -1;
     }
 
-    nst_video_set_dimensions_screen(nst_video_get_dimensions_screen());
+    nst_video_set_dimensions_screen(
+        nst_video_get_dimensions_screen()
+    );
 
     // Set play in motion
     nst_play();
@@ -330,14 +486,17 @@ int PNESUiEmu::nestopia_core_init(const char *rom_path) {
 }
 
 int nestopia_state_load(const char *path) {
-
     int ret = -1;
+
     std::filebuf fb;
 
     if (fb.open(path, std::ios::in)) {
         std::istream is(&fb);
+
         Machine machine(emulator);
+
         ret = machine.LoadState(is);
+
         fb.close();
     }
 
@@ -345,20 +504,39 @@ int nestopia_state_load(const char *path) {
 }
 
 int nestopia_state_save(const char *path) {
-
     int ret;
+
     std::ofstream ofs;
 
-    ofs.open(path, std::ofstream::out | std::ofstream::binary);
+    ofs.open(
+        path,
+        std::ofstream::out |
+        std::ofstream::binary
+    );
+
     Machine machine(emulator);
-    ret = machine.SaveState(ofs, Nes::Api::Machine::NO_COMPRESSION);
+
+    ret = machine.SaveState(
+        ofs,
+        Nes::Api::Machine::NO_COMPRESSION
+    );
+
     ofs.close();
 
     return ret;
 }
 
-void nst_input_turbo_pulse(Nes::Api::Input::Controllers *controllers) {}
+// These callbacks are retained because other parts of
+// the Nestopia frontend expect them to exist.
+// Turbo input for Switch is implemented directly
+// in PNESUiEmu::onUpdate().
+void nst_input_turbo_pulse(
+    Nes::Api::Input::Controllers *controllers
+) {
+}
 
-void nst_input_turbo_init() {}
+void nst_input_turbo_init() {
+}
 
-void nst_input_init() {}
+void nst_input_init() {
+}
